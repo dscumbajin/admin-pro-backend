55d23ae4d7290f44ce0c6d355f4a2386e17be2be
dscumbajin <dscumbajin@uce.edu.ec>
2020-09-13 12:42:10 -0500

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 const express = require('express');
 const { dbConnection } = require('./database/config');
 const cors = require('cors');
@@ -25,6 +26,11 @@ app.use('/api/medicos', require('./routes/medicos.route'));
 app.use('/api/login', require('./routes/auth.route'));
 app.use('/api/todo', require('./routes/busquedas.route'));
 app.use('/api/upload', require('./routes/uploads.route'));
+
+// Lo ultimo
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Servidor corriendo en el puerto: ' + process.env.PORT);
